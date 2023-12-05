284bb0154a3c9c96ae71b1a697ce4bc393815a59
Zhengyi Jiang <anthony.jiang.github@outlook.com>
2023-12-05 14:07:00 +0000

    service server instead of service client!

--- unitree_legged_real/src/exe/unitree_high_level.cpp
+++ unitree_legged_real/src/exe/unitree_high_level.cpp
@@ -53,7 +53,7 @@ UnitreeHighLevel unitree;
 ros::Subscriber sub_cmd_vel;
 ros::Publisher pub_high_state;
 ros::Publisher pub_imu;
-ros::ServiceClient mode_srv_client;
+ros::ServiceServer mode_srv;
 
 void publishIMU()
 {
@@ -116,7 +116,7 @@ int main(int argc, char **argv)
     sub_cmd_vel = nh.subscribe("cmd_vel", 1, cmdVelCallback);
     pub_high_state = nh.advertise<unitree_legged_msgs::HighState>("high_state", 1);
     pub_imu = nh.advertise<sensor_msgs::Imu>("imu/data", 1);
-    mode_srv_client = nh.serviceClient<unitree_legged_msgs::SetUnitreeHLMode>("set_unitree_hl_mode");
+    mode_srv = nh.advertiseService("set_unitree_high_level_mode", modeSrvCallback));
 
     LoopFunc loop_imuPub("imu", 0.05, 3, publishIMU);
     LoopFunc loop_highStatePub("high_state", 0.05, 3, publishHighState);
